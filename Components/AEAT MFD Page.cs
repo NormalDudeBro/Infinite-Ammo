30a96f94b85e30f1c9cfd072dca8c64637c6bf2f
NormalDudeBro <68166174+NormalDudeBro@users.noreply.github.com>
2022-09-30 17:08:23 -0700

    Update AEAT MFD Page.cs
    removed paired guns and big rpm

--- Components/AEAT MFD Page.cs
+++ Components/AEAT MFD Page.cs
@@ -19,13 +19,7 @@ public class AEATMFDPage : MonoBehaviour
         AEATDebugLogger.Log("Do AEAT Page buttons");
         inf = playerVehicle.AddComponent<InfiniteAmmo>();
         Image infAmmoImage = transform.Find("Infinite Ammo").Find("bg").GetComponent<Image>();
-        pGuns = playerVehicle.AddComponent<PairedGuns>();
-        Image pGunsImage = transform.Find("Paired Guns").Find("bg").GetComponent<Image>();
-        rpm = playerVehicle.AddComponent<BigRPM>();
-        Image rpmImage = transform.Find("Big RPM").Find("bg").GetComponent<Image>();
         inf.enabled = false;
-        pGuns.enabled = false;
-        rpm.enabled = false;
         page = GetComponent<MFDPage>();
         MFDPage.MFDButtonInfo infButton = new MFDPage.MFDButtonInfo();
         infButton.button = MFD.MFDButtons.L3;
@@ -39,31 +33,7 @@ public class AEATMFDPage : MonoBehaviour
             else
                 infAmmoImage.color = new Color(69, 0, 0);
         });
-        MFDPage.MFDButtonInfo pGunsButton = new MFDPage.MFDButtonInfo();
-        pGunsButton.button = MFD.MFDButtons.L4;
-        pGunsButton.toolTip = "If one gun fires, all guns fire";
-        pGunsButton.label = "Paired Guns";
-        pGunsButton.OnPress.AddListener(delegate
-        {
-            pGuns.enabled = !pGuns.enabled;
-            if (pGuns.enabled)
-                pGunsImage.color = new Color(0, 69, 0);
-            else
-                pGunsImage.color = new Color(69, 0, 0);
-        });
-        MFDPage.MFDButtonInfo rpmButton = new MFDPage.MFDButtonInfo();
-        rpmButton.button = MFD.MFDButtons.L5;
-        rpmButton.toolTip = "Guns shoot really fast";
-        rpmButton.label = "Big RPM";
-        rpmButton.OnPress.AddListener(delegate
-        {
-            rpm.enabled = !rpm.enabled;
-            if (rpm.enabled)
-                rpmImage.color = new Color(0, 69, 0);
-            else
-                rpmImage.color = new Color(69, 0, 0);
-        });
-        page.buttons = new MFDPage.MFDButtonInfo[] { infButton, pGunsButton, rpmButton };
+        page.buttons = new MFDPage.MFDButtonInfo[] { infButton };
         //page.buttons.AddToArray(infButton);
         //page.buttons.AddToArray(pGunsButton);
         //page.buttons.AddToArray(rpmButton);
@@ -79,13 +49,7 @@ public class AEATMFDPage : MonoBehaviour
                 return;
             inf = playerVehicle.AddComponent<InfiniteAmmo>();
             Image infAmmoImage = transform.Find("Infinite Ammo").Find("bg").GetComponent<Image>();
-            pGuns = playerVehicle.AddComponent<PairedGuns>();
-            Image pGunsImage = transform.Find("Paired Guns").Find("bg").GetComponent<Image>();
-            rpm = playerVehicle.AddComponent<BigRPM>();
-            Image rpmImage = transform.Find("Big RPM").Find("bg").GetComponent<Image>();
             inf.enabled = false;
-            pGuns.enabled = false;
-            rpm.enabled = false;
             page = GetComponent<MFDPage>();
             MFDPage.MFDButtonInfo infButton = new MFDPage.MFDButtonInfo();
             infButton.button = MFD.MFDButtons.L3;
@@ -99,31 +63,7 @@ public class AEATMFDPage : MonoBehaviour
                 else
                     infAmmoImage.color = new Color(69, 0, 0);
             });
-            MFDPage.MFDButtonInfo pGunsButton = new MFDPage.MFDButtonInfo();
-            pGunsButton.button = MFD.MFDButtons.L4;
-            pGunsButton.toolTip = "If one gun fires, all guns fire";
-            pGunsButton.label = "Paired Guns";
-            pGunsButton.OnPress.AddListener(delegate
-            {
-                pGuns.enabled = !pGuns.enabled;
-                if (pGuns.enabled)
-                    pGunsImage.color = new Color(0, 69, 0);
-                else
-                    pGunsImage.color = new Color(69, 0, 0);
-            });
-            MFDPage.MFDButtonInfo rpmButton = new MFDPage.MFDButtonInfo();
-            rpmButton.button = MFD.MFDButtons.L5;
-            rpmButton.toolTip = "Guns shoot really fast";
-            rpmButton.label = "Big RPM";
-            rpmButton.OnPress.AddListener(delegate
-            {
-                rpm.enabled = !rpm.enabled;
-                if (rpm.enabled)
-                    rpmImage.color = new Color(0, 69, 0);
-                else
-                    rpmImage.color = new Color(69, 0, 0);
-            });
-            page.buttons = new MFDPage.MFDButtonInfo[] { infButton, pGunsButton, rpmButton };
+            page.buttons = new MFDPage.MFDButtonInfo[] { infButton };
             //page.buttons.AddToArray(infButton);
             //page.buttons.AddToArray(pGunsButton);
             //page.buttons.AddToArray(rpmButton);
@@ -133,7 +73,5 @@ public class AEATMFDPage : MonoBehaviour
 
     private bool addedPage = false;
     private InfiniteAmmo inf;
-    private PairedGuns pGuns;
-    private BigRPM rpm;
     private MFDPage page;
 }
